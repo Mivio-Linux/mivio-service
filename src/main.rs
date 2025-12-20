bfffeed4a5cb0504ea47a230c96652792721fbf1
MivioDev <miviodev@gmail.com>
2025-12-20 18:37:28 +0300

    add fn get_dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     let print_usage = ||{
         print!("USAGE:\n ms add [SERVICE]     -  add service\n ms remove [SERVICE]  -  remove service\n ms list              -  list all services\n") //функция, где пишем сообщение как использовать
@@ -6,9 +7,9 @@ fn main() {
     if arguments.len() == 2 && arguments[1] == "list".to_string() {  list_services()  }// если 2 аргумента и 2аргумент равен list то пишем все сервисы
     else if arguments.len() == 3 { // иначе если 3 аргумента И
         if arguments[1] == "add" { // если 2 аргумент add то
-            add_service(arguments[2].clone()) //берем 3 аргумент и добавляем сервис
+            add_service(&arguments[2]) //берем 3 аргумент и добавляем сервис
         } else if arguments[1] == "remove"{ // если 2 аргумент remove ещ
-            remove_service(arguments[2].clone()) // удаляем сервис
+            remove_service(&arguments[2]) // удаляем сервис
         } else {
             print_usage() // иначе выведи как использовать
         }
@@ -17,17 +18,15 @@ fn main() {
         print_usage() // если не 2 и не 3 аргумента то выведи использование
     }
 }
-fn add_service(service: String) {
+fn list_dir(s: String) -> Vec<String>{
+    let files: Vec<String> = std::fs::read_dir(s).unwrap().map(|e| e.unwrap().file_name().into_string().unwrap()).collect();
+    files
+}
+fn add_service(service: &String) {
     println!("Adding Service..."); 
-    let files: Vec<String> = std::fs::read_dir("/etc/sv/") //читаем директорию
-    .unwrap() // 
-    .map(|e| e.unwrap().file_name().into_string().unwrap())
-    .collect();
+    let files: Vec<String> = list_dir(String::from("/etc/sv/"));
 
-    let services: Vec<String> = std::fs::read_dir("/var/service/") // читаем директорию 2
-    .unwrap()
-    .map(|e| e.unwrap().file_name().into_string().unwrap())
-    .collect();
+    let services: Vec<String> = list_dir(String::from("/var/service/"));
     if files.contains(&service) && !services.contains(&service)  { // если файл с названием сервиса
                                                                    // есть в дир1 но нет в дир2 то
         let mut path = String::new(); // создаем строку
@@ -50,13 +49,9 @@ fn add_service(service: String) {
         println!("[ERROR] check /etc/sv or /var/service");
     }
 }
-fn remove_service(service: String) {
+fn remove_service(service: &String) {
     println!("Removing...");
-    let services: Vec<String> = std::fs::read_dir("/var/service/")
-    .unwrap()
-    .map(|e| e.unwrap().file_name().into_string().unwrap())
-    .collect();
-
+    let services: Vec<String> = list_dir(String::from("/var/service/"));
     if services.contains(&service) {
         let mut path = String::new();
         path.push_str("/var/service/");
@@ -67,10 +62,7 @@ fn remove_service(service: String) {
 }
 }
 fn list_services() {
-    let services: Vec<String> = std::fs::read_dir("/etc/sv/")
-    .unwrap()
-    .map(|e| e.unwrap().file_name().into_string().unwrap())
-    .collect();
+    let services: Vec<String> = list_dir(String::from("/etc/sv/"));
     for service in services {
         println!("{}", service);
     }
